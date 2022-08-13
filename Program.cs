f738c106b7df34dfaff510e0b5aeb101b88a45ab
Wahab <Wahab@DESKTOP-C2N6EBP>
2022-08-14 01:47:58 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InterviewQuestion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
